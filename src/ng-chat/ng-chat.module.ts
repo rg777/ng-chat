5f1ed562409407f6d1a161066abcd26eb363c439
rg777 <rachit25793@gmail.com>
2018-02-10 16:24:47 +0900

    Update ng-chat.module.ts

--- src/ng-chat/ng-chat.module.ts
+++ src/ng-chat/ng-chat.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import {MatCardModule} from '@angular/material/card';
 import { NgChat } from './ng-chat.component';
 import { EmojifyPipe } from './pipes/emojify.pipe';
 import { LinkfyPipe } from './pipes/linkfy.pipe';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatCardModule],
   declarations: [NgChat, EmojifyPipe, LinkfyPipe],
   exports: [NgChat]
 })
