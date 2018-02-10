0de79e88a1e9653ba3737404a8e1aacc4b6f7b06
rg777 <rachit25793@gmail.com>
2018-02-10 16:31:31 +0900

    Update message.ts

--- src/ng-chat/core/message.ts
+++ src/ng-chat/core/message.ts
@@ -3,5 +3,18 @@ export class Message
     public fromId: any;
     public toId: any;
     public message: string;
+    public card?: {
+        date?: string;
+        timing?: string;
+        locations?: any;
+        confidence?: string;
+        color?: string;
+        raw_data?: any;
+        selected?: boolean
+        subject?: string;
+        message?: string;
+        booked?: boolean;
+        participants?: any;
+    };
     public seenOn?: Date;
 }
